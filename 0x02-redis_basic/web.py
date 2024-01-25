171f77141360badfcc5d2aada199f2ba01169871
Ardiy0011 <ardiyquarsh@gmail.com>
2024-01-25 00:22:45 -0800

    redis base class

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" this module contains the function for task 0 """
+
+from functools import wraps
+import redis
+import requests
+from typing import Callable
+
+redis_ = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        ht = method(url)
+        redis_.setex(f"cached:{url}", 10, ht)
+        return ht
+
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
